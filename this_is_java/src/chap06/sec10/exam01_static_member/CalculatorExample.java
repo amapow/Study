b8ea1f233404d3cc37530c141072a66e76a2ebc9
janghyeon.jung <janghyeon.jung@lgepartner.com>
2019-02-12 20:32:35 +0900

    이것이 자바다 6절 10

---
+++ this_is_java/src/chap06/sec10/exam01_static_member/CalculatorExample.java
@@ -0,0 +1,14 @@
+package chap06.sec10.exam01_static_member;
+
+public class CalculatorExample {
+    public static void main(String[] args) {
+        double result1 = 10 * 10 * Calculator.pi;
+
+        int result2 = Calculator.plus(1, 2);
+        int result3 = Calculator.minus(1, 2);
+
+        System.out.println("result1 = " + result1);
+        System.out.println("result2 = " + result2);
+        System.out.println("result3 = " + result3);
+    }
+}
